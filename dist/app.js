3d676cfb006cbef61ea5e42dc4d8a39ae68c43fc
Asmaa26189 <engasmaahesham@gmail.com>
2024-12-02 00:16:28 +0200

    updates for vercel

--- dist/app.js
+++ dist/app.js
@@ -17,14 +17,32 @@ app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 // MongoDB Connection
 (0, connectDB_1.default)();
+const whitelist = [
+    '*'
+];
+app.use((req, res, next) => {
+    const origin = req.get('referer');
+    const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
+    if (isWhitelisted) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
+        res.setHeader('Access-Control-Allow-Credentials', "true");
+    }
+    // Pass to next layer of middleware
+    if (req.method === 'OPTIONS')
+        res.sendStatus(200);
+    else
+        next();
+});
 // Routes
 app.use('/api/user', require('./routes/user').default);
 app.use('/api/car', require('./routes/car').default);
 app.use('/api/tracking', require('./routes/carTracking').default);
 app.use('/api/owner', require('./routes/owner').default);
-app.use((req, res) => {
-    res.status(404).json({ success: false, message: 'Route not found' });
-});
+// app.use((req, res) => {
+//   res.status(404).json({ success: false, message: 'Route not found' });
+// });
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
