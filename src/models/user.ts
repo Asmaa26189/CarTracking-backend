a0624e6725b57f368378cbf94ef2f8eb673371bd
Asmaa26189 <engasmaahesham@gmail.com>
2025-02-17 15:35:04 +0200

    updates

--- src/models/user.ts
+++ src/models/user.ts
@@ -10,7 +10,7 @@ export interface IUser {
 
 const UserSchema = new Schema<IUser>({
   name: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true ,unique: true},
   type: { type: String, required: true, default: 'Admin'},
   password: { type: String, required: true },
 });
